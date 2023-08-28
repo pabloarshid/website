08ec251ab1392b48612e5a50bd8f631f099efb5c
pabloarshid <umarsa.birvancity@gmail.com>
2023-08-27 23:16:57 -0700

    working on a samll game to help understand react and the framework

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import './component/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Square( { value, onSquareClick } : {value: string, onSquareClick: any}){
+  
+  return( 
+    <button 
+      className="square"
+      onClick={onSquareClick}
+      >{value}</button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [squares, setSquares] = useState<any>(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+  return (
+  <> 
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={handleClick} />
+      <Square value={squares[1]} onSquareClick={handleClick}/>
+      <Square value={squares[2]} onSquareClick={handleClick}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={handleClick}/>
+      <Square value={squares[4]} onSquareClick={handleClick}/>
+      <Square value={squares[5]} onSquareClick={handleClick}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} onSquareClick={handleClick}/>
+      <Square value={squares[7]} onSquareClick={handleClick}/>
+      <Square value={squares[8]} onSquareClick={handleClick}/>
+    </div>
+  </>
+     
+  );
+
+  
+}
+
+// export default App;
